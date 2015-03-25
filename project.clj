bd6381c0fbea972298d30e4c35586883c1364e13
Mark Woodhall <mark.woodhall@gmail.com>
2015-03-25 22:30:31 +0000

    Initial commit

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject clj-ravendb-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An example project that shows how to use the clj-ravendb client"
+  :url "http://github.com/markwoodhall/clj-ravendb-example"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
+                 [clj-ravendb "0.9.0"]
+                 [cheshire "5.4.0"]
+                 [de.ubercode.clostache/clostache "1.4.0"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler clj-ravendb-example.handler/app}
--- src/clj_ravendb_example/handler.clj
+++ src/clj_ravendb_example/handler.clj
@@ -1,11 +1,58 @@
 (ns clj-ravendb-example.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :refer [generate-string]]
+            [clostache.parser :refer [render-resource]]
+            [clj-ravendb.client :as rdb]))
+
+(def ravendb-url "https://markwoodhall-cg5w.ravenhq.com")
+(def ravendb-database "markwoodhall-clj-ravendb")
+
+(def oauth-url "https://amazon-useast-2-oauth.ravenhq.com")
+(def api-key "0482ed02-a7bf-409a-af30-ea3c177d1465")
+
+(defn load-employee
+  [id]
+  #(let [client (rdb/client ravendb-url ravendb-database {:ssl-insecure? true :oauth-url oauth-url :api-key api-key})]
+     (first (:results (rdb/load-documents client [(str "employees/" id)])))))
+
+(defn load-employees
+  []
+  #(let [client (rdb/client ravendb-url ravendb-database {:ssl-insecure? true :oauth-url oauth-url :api-key api-key})]
+     (:results (rdb/query-index client {:index "Raven/DocumentsByEntityName" :Tag "Employees"}))))
+
+(defn employee-view
+  [populate-with-fn]
+  #(render-resource "templates/employee.mustache" (populate-with-fn)))
+
+(defn employees-view
+  [populate-with-fn]
+  #(render-resource "templates/employees.mustache" {:employees (populate-with-fn)}))
+
+(defn master-view
+  [populate-with-fn]
+  #(render-resource "templates/index.mustache" {:content (populate-with-fn)}))
+
+(defn respond
+  [with-function]
+  (with-function))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+       (-> (load-employees)
+           (employees-view)
+           (master-view)
+           (respond)))
+  (GET "/employees/:id" [id]
+       (-> id
+           (load-employee)
+           (employee-view)
+           (master-view)
+           (respond)))
+
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes site-defaults)) ()
